493cdbc4aa5d79c04e9ef2695d105597c1b0cb11
ihtisham-sudo <ahsaanullah174@gmail.com>
2025-03-09 22:50:14 +0500

    first commit

---
+++ MVC-Ihtisham-1078/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MVC_Ihtisham_1078.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
